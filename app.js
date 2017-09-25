22ec1cf16bbc4201ea38efb818a54464ce493913
EdenRefined <EllieEvdV@gmail.com>
2017-09-25 15:37:51 +0200

    players no longer allowed to play the same square

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var nextPlayer = true;
 
+var boardState = [
+	null, null, null,
+	null, null, null,
+	null, null, null
+];
+
 function play(box) {
-	if(nextPlayer) {
-		box.innerHTML = 'x';
-	} else{
-		box.innerHTML = 'o';
-	}
-	nextPlayer = !nextPlayer;
-}
+	if(boardState[box.id] == null) {
+		if(nextPlayer) {
+			box.innerHTML = 'x';
+		} else{
+			box.innerHTML = 'o';
+		}
+		nextPlayer = !nextPlayer;
+	} else {
+		alert('Helloo?? Are you looking? You can\'t do that!')
+	};
+	console.log(boardState);
+
+};
+	
