372ad93e0224c5df630f53a44a39596a647bbfe1
EdenRefined <EllieEvdV@gmail.com>
2017-10-04 12:14:40 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
+var controller = {
+	models: {},
+	view: {},
+	addModel: function(newModel) {
+		this.models[newModel.name] = newModel;
+	},
+	setView: function(newView){
+		this.view = newView;
+	},
+	play: function(box){
+		var row = box.parentElement.getAttribute('row');
+		var column = box.getAttribute('column');
+		var nextPlayer = this.models.playerState.getNextPlayer();
+		this.models.boardState.addMove(row, column, nextPlaer, (err) => {
+			if(err) {
+				alert('Hello?!?! Are you paying attention? You can\'t go there!');
+			} else {
+				var newBoard = this.models.boardState.getBoardState();
+				this.view.repopulateBoard(newBoard);
+				this.models.playerState.togglePlater();
+			}
+		});
+	}
+};
+
 function initializeApp() {
+	console.log('hi');
+	boardModel.setBoardState([
+			[null, null, null],
+			[null, null, null],
+			[null, null, null]
+	]);
+	var firstPlayer = true;
+	playerModel.setPlayerState(firstPlayer);
 	controller.addModel(boardModel);
 	controller.addModel(playerModel);
-	controller.setView(view);
+	controller.setView(gameplayViewBrowser);
 	initializeView.buildBoard();
-	console.log('hi')
 };
